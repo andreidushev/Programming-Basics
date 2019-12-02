c564fd6271e22a3611812544da2ef2342a0d5242
Andrei Dushev <andrei4o6@abv.bg>
2019-12-02 16:46:26 +0200

    add Histogram

--- Loops/04. Histogram/Histogram.cs
+++ Loops/04. Histogram/Histogram.cs
@@ -2,11 +2,49 @@
 
 namespace _04._Histogram
 {
-    class Program
+    class Histogram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            double p1 = 0;
+            double p2 = 0;
+            double p3 = 0;
+            double p4 = 0;
+            double p5 = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (num < 200)
+                {
+                    p1++;
+                }
+                else if (num <= 399)
+                {
+                    p2++;
+                }
+                else if (num <= 599)
+                {
+                    p3++;
+                }
+                else if (num <= 799)
+                {
+                    p4++;
+                }
+                else
+                {
+                    p5++;
+                }
+            }
+
+            Console.WriteLine($"{p1 / n * 100:F2}%");
+            Console.WriteLine($"{p2 / n * 100:F2}%");
+            Console.WriteLine($"{p3 / n * 100:F2}%");
+            Console.WriteLine($"{p4 / n * 100:F2}%");
+            Console.WriteLine($"{p5 / n * 100:F2}%");
         }
     }
 }
