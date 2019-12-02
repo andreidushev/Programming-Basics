71833806f1f81e3bcfce266f31a2b3a9ac7dbb04
Andrei Dushev <andrei4o6@abv.bg>
2019-12-02 14:01:36 +0200

    add Numbers to 1000 edn 7s

---
+++ Loops/01. Numbers-to-1000-end-with-7/NumbersEndWith7.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01._Numbers_to_1000_end_with_7
+{
+    class NumbersEndWith7
+    {
+        static void Main()
+        {
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 10 == 7)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
