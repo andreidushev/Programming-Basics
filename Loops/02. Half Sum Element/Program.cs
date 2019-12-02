bb8ea2ca0c60c4a71f6a94d58c28709924cfe16b
Andrei Dushev <andrei4o6@abv.bg>
2019-12-02 14:08:57 +0200

    02. Half Sum Element

---
+++ Loops/02. Half Sum Element/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _02._Half_Sum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int maxNum = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+
+                if (maxNum < num)
+                {
+                    maxNum = num;
+                }
+            }
+
+            int sumWithoutMaxNum = sum - maxNum;
+
+            if (sumWithoutMaxNum == maxNum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {maxNum}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(sumWithoutMaxNum - maxNum)}");
+            }
+        }
+    }
+}
