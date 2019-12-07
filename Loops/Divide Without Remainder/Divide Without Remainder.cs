50fd6898c0fd52f68dfa75bf78b1a388270804ef
Andrei Dushev <andrei4o6@abv.bg>
2019-12-07 14:01:39 +0200

    add 05. Divide Without Remainder

--- Loops/Divide Without Remainder/Divide Without Remainder.cs
+++ Loops/Divide Without Remainder/Divide Without Remainder.cs
@@ -6,7 +6,32 @@ namespace Divide_Without_Remainder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            double p1 = 0;
+            double p2 = 0;
+            double p3 = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (num % 2 == 0)
+                {
+                    p1++;
+                }
+                if (num % 3 == 0)
+                {
+                    p2++;
+                }
+                if (num % 4 == 0)
+                {
+                    p3++;
+                }
+            }
+            Console.WriteLine($"{p1 * 1.0 / n * 100:F2}%");
+            Console.WriteLine($"{p2 * 1.0 / n * 100:F2}%");
+            Console.WriteLine($"{p3 * 1.0 / n * 100:F2}%");
         }
     }
 }
