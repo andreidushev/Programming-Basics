f4814a2f25a3a9e2f3503990d56325009b8a8284
Andrei Dushev <andrei4o6@abv.bg>
2019-12-08 13:00:34 +0200

    add Sum of Two Numbers

---
+++ Loops/04. Sum of Two Numbers/Sum of Two Numbers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _04._Sum_of_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            bool hasMagical = false;
+            int counter = 0;
+
+            for (int i = firstNumber; i <= secondNumber; i++)
+            {
+                for (int j = firstNumber; j <= secondNumber; j++)
+                {
+                    counter++;
+
+                    int sum = i + j;
+
+                    if (sum == magicNumber)
+                    {
+                        Console.WriteLine($"Combination N:{counter} ({i} + {j} = {sum})");
+                        hasMagical = true;
+                        break;
+                    }
+                }
+                if (hasMagical)
+                {
+                    break;
+                }
+            }
+            if (!hasMagical)
+            {
+                Console.WriteLine($"{counter} combinations - neither equals {magicNumber}");
+            }
+        }
+    }
+}
