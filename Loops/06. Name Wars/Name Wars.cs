176e29437d7716a51100952ab71998493ff72ef6
Andrei Dushev <andrei4o6@abv.bg>
2019-12-08 15:47:26 +0200

    add 07. Building

--- Loops/06. Name Wars/Name Wars.cs
+++ Loops/06. Name Wars/Name Wars.cs
@@ -6,7 +6,32 @@ namespace _06._Name_Wars
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int current = 0;
+            int max = 0;
+            string namemax = "";
+            int max1 = int.MinValue;
+
+            while (true)
+            {
+
+                string name = Console.ReadLine();
+
+                if (name == "STOP")
+                    break;
+
+
+                for (int i = 0; i < name.Length; i++)
+                    current = current + name[i];
+
+                if (current > max1 && current > max)
+                {
+                    max = current;
+                    namemax = name;
+                }
+                current = 0;
+
+            }
+            Console.WriteLine($"Winner is {namemax} - {max}!");
         }
     }
 }
