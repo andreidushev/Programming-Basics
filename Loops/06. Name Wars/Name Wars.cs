b05a7d2f19f56598e5fbd04d6b6aa1a1b5d44141
Andrei Dushev <andrei4o6@abv.bg>
2019-12-08 13:51:05 +0200

    add Name Wars

---
+++ Loops/06. Name Wars/Name Wars.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06._Name_Wars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
