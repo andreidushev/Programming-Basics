2b61af94040461f95770430ec5ce5edec209200a
Andrei Dushev <andrei4o6@abv.bg>
2019-12-08 13:41:54 +0200

    add Travelling

---
+++ Loops/05. Travelling/05. Travelling.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _05._Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+
+            while (destination != "End")
+            {
+                double savedMoney = 0;
+                double budget = double.Parse(Console.ReadLine());
+
+                while (savedMoney < budget)
+                {
+                    double moneyIN = double.Parse(Console.ReadLine());
+                    savedMoney += moneyIN;
+                }
+                Console.WriteLine($"Going to {destination}!");
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}
