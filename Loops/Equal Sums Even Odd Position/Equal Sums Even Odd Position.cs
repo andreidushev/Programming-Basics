12aab033ffc2023c58947fc12adf0e4c00dfa6c1
Andrei Dushev <andrei4o6@abv.bg>
2019-12-09 16:19:29 +0200

    add Equal Sums Even Odd Position

---
+++ Loops/Equal Sums Even Odd Position/Equal Sums Even Odd Position.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Equal_Sums_Even_Odd_Position
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            for (int i = firstNum; i <= secondNum; i++)
+            {
+                string currentNum = i.ToString();
+                int sumOdd = 0;
+                int sumEven = 0;
+
+                for (int j = 0; j < currentNum.Length; j++)
+                {
+                    int currentDigit = int.Parse(currentNum[j].ToString());
+                    if (j % 2 == 0)
+                    {
+                        sumEven += currentDigit;
+                    }
+                    else
+                    {
+                        sumOdd += currentDigit;
+                    }
+                }
+                if (sumOdd == sumEven)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
