f1cc3d159974ff16c014e18610429afe931cb3ff
Andrei Dushev <andrei4o6@abv.bg>
2019-12-09 16:07:03 +0200

    add Coding

---
+++ Loops/Coding/Coding.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Coding
+{
+    class Coding
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            for (int i = input.Length - 1; i >= 0 ; i--)
+            {
+                char currentDigit = input[i];
+                int currentDigitToNumber = int.Parse(currentDigit.ToString());
+
+                if (currentDigitToNumber == 0)
+                {
+                    Console.WriteLine("ZERO");
+                    continue;
+                }
+                for (int n = 1; n <= currentDigitToNumber; n++)
+                {
+                    Console.Write((char)(currentDigitToNumber + 33));
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
