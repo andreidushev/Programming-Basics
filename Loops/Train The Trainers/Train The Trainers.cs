5d9753e8474ca8fe59000b5ca096863036817d94
Andrei Dushev <andrei4o6@abv.bg>
2019-12-10 13:22:11 +0200

    add Train The Trainers

---
+++ Loops/Train The Trainers/Train The Trainers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Train_The_Trainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int juryCount = int.Parse(Console.ReadLine());
+            string presentation = Console.ReadLine();
+
+            double totalMark = 0;
+            int counter = 0;
+
+            while (presentation != "Finish")
+            {
+                double sumMarks = 0;
+                for (int i = 1; i <= juryCount ; i++)
+                {
+                    double mark = double.Parse(Console.ReadLine());
+                    sumMarks += mark;
+                }
+                double grade = sumMarks / juryCount;
+                totalMark += grade;
+                Console.WriteLine($"{presentation} - {grade:F2}.");
+                counter++;
+                presentation = Console.ReadLine();
+            }
+            double avarageGrade = totalMark / counter;
+            Console.WriteLine($"Student's final assessment is {avarageGrade:F2}.");
+        }
+    }
+}
